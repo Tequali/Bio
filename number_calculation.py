0830cbc116de0f8fe1a6f6c6e6e2482a1219275e
Tequali <dschuhknecht17@gmail.com>
2024-08-28 15:26:03 +0200

    number_calc is now showing prime numbers

--- number_calculation.py
+++ number_calculation.py
@@ -8,3 +8,16 @@ if number % 2 == 0:
     print(f"{number} is an even number.")
 else:
     print(f"{number} is an odd number.")
+
+# Check if the number is prime
+
+def prime_number(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5)+1):
+        if number % i == 0:
+            return False
+        return True
+
+if prime_number(number):
+    print(f"{number} is a prime number.")
