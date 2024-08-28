9f7e665a54499693467e7064101e30cf6a6062dd
Tequali <dschuhknecht17@gmail.com>
2024-08-28 15:38:35 +0200

    number_calc will tell if its squared or not

--- number_calculation.py
+++ number_calculation.py
@@ -1,3 +1,4 @@
+import math
 number = input("Enter a number: ")
 
 # Convert the input to an integer
@@ -23,3 +24,13 @@ if prime_number(number):
     print(f"{number} is a prime number.")
 else:
     print(f"{number} is not a prime number.")
+
+# Check if the number is a perfect square
+
+def is_perfect_square(number):
+    return math.isqrt(number)**2 == number
+
+if is_perfect_square(number):
+    print(f"{number} is a perfect square.")
+else:
+    print(f"{number} is not a perfect square.")
